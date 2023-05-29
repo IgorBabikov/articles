a4f828ca28b1b118f8bb9d4c998b6e4ad4771948
IgorBabikov <igorbabikov91@gmail.com>
2023-05-29 17:18:39 +0300

    added jest test

---
+++ src/shared/lib/classNames/classNames.test.ts
@@ -0,0 +1,42 @@
+import { classNames } from './classNames';
+
+describe('sum module', () => {
+	test('width only first para', () => {
+		expect(classNames('someClass')).toBe('someClass');
+	});
+
+	test('width additional class', () => {
+		const expected = 'mainClass class1 class2';
+		expect(classNames('mainClass', ['class1 class2'], {})).toBe(expected);
+	});
+
+	test('width mods', () => {
+		const expected = 'mainClass class1 class2 active hovered';
+		expect(
+			classNames('mainClass', ['class1 class2'], {
+				active: true,
+				hovered: true,
+			})
+		).toBe(expected);
+	});
+
+	test('width mods false', () => {
+		const expected = 'mainClass class1 class2 active';
+		expect(
+			classNames('mainClass', ['class1 class2'], {
+				active: true,
+				hovered: false,
+			})
+		).toBe(expected);
+	});
+
+	test('width mods false', () => {
+		const expected = 'mainClass class1 class2 active';
+		expect(
+			classNames('mainClass', ['class1 class2'], {
+				active: true,
+				hovered: undefined,
+			})
+		).toBe(expected);
+	});
+});
